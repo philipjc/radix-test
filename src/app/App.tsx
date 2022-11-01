de34009b7342e73011250517bdf7abf5644dea64
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-11-01 19:24:26 +0000

    header compo. dir changes. prettier

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import {MainNavigation} from "../components/navigation/MainNavigation";
+
+function App() {
+  return (
+    <div className="App">
+      <MainNavigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigation/MainNavigation.tsx
@@ -0,0 +1,119 @@
+import React, { ReactElement } from 'react';
+
+export function MainNavigation(): ReactElement {
+  return (
+    <section className="section">
+      <div className="column is-10 ml-auto mr-auto">
+        <nav
+          className="navbar"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div className="navbar-brand">
+            <a
+              className="navbar-item"
+              href="https://bulma.io"
+            >
+              <img
+                src="https://bulma.io/images/bulma-logo.png"
+                width="112"
+                height="28"
+                alt="alt text"
+              />
+            </a>
+
+            <a
+              href="/"
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div
+            id="navbarBasicExample"
+            className="navbar-menu"
+          >
+            <div className="navbar-start">
+              <a
+                href="/"
+                className="navbar-item"
+              >
+                Home
+              </a>
+
+              <a
+                href="/"
+                className="navbar-item"
+              >
+                Documentation
+              </a>
+
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a
+                  href="/"
+                  className="navbar-link"
+                >
+                  More
+                </a>
+
+                <div className="navbar-dropdown">
+                  <a
+                    href="/"
+                    className="navbar-item"
+                  >
+                    About
+                  </a>
+                  <a
+                    href="/"
+                    className="navbar-item"
+                  >
+                    Jobs
+                  </a>
+                  <a
+                    href="/"
+                    className="navbar-item"
+                  >
+                    Contact
+                  </a>
+                  <hr className="navbar-divider" />
+                  <a
+                    href="/"
+                    className="navbar-item"
+                  >
+                    Report an issue
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="buttons">
+                  <a
+                    href="/"
+                    className="button is-primary"
+                  >
+                    <strong>Sign up</strong>
+                  </a>
+                  <a
+                    href="/"
+                    className="button is-light"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
+}

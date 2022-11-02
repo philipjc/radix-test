505242206066cff6a54db34c74fc474b3120cf50
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-11-02 12:33:01 +0000

    Update main-navigation.styled.tsx

--- src/components/navigation/main-navigation.styled.tsx
+++ src/components/navigation/main-navigation.styled.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-const SectionStyled = styled.section``;
+const SectionStyled = styled.section`
+  border-bottom: 2px solid ${props => props.theme.colors.blue3};
+`;
 
 const MainNavigationStyles = {
   SectionStyled,

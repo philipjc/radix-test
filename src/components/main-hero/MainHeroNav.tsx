df509dfbfa13377890300d669075b9b5e014c2ee
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-11-05 22:22:58 +0000

    event for meal of category. broke category nav style.

--- src/components/main-hero/MainHeroNav.tsx
+++ src/components/main-hero/MainHeroNav.tsx
@@ -10,53 +10,57 @@ export function MainHeroNav() {
     food: { fetchMeals },
   } = useMeals();
 
-  const veg = categories.categories
-    ? categories.categories.find((cat: iCategory) => cat.strCategory === 'Vegetarian')
-    : null;
+  const categoryList = categories.categories
+    ? categories.categories.map((cat: iCategory) => cat.strCategory)
+    : [];
 
   return (
     <nav className="tabs is-boxed is-fullwidth">
       <div className="container">
         <ul className="is-flex is-justify-content-space-evenly">
-          <li className="is-active is-flex-grow-1">
-            {categories?.fetching ? (
-              <button
-                className="button"
-                style={{
-                  width: '100%',
-                  border: 'none',
-                  borderRadius: 0,
-                  cursor: 'pointer',
-                }}
-              >
-                <Puff
-                  height="20"
-                  width="20"
-                  radius={1}
-                  color="#0f3058"
-                  ariaLabel="puff-loading"
-                  wrapperStyle={{ display: 'flex', justifyContent: 'center' }}
-                  wrapperClass=""
-                  visible={true}
-                />
-              </button>
-            ) : (
-              <Link to={'/'}>
-                <button
-                  className="button"
-                  onClick={() => fetchMeals()}
-                  style={{
-                    width: '100%',
-                    border: 'none',
-                    borderRadius: 0,
-                    cursor: 'pointer',
-                  }}
-                >
-                  {veg?.strCategory}
-                </button>
-              </Link>
-            )}
-          </li>
+          {categoryList.map((cat: string, idx: number) => {
+            return (
+              <li className="is-active is-flex-grow-1" key={`${cat}-${idx}`}>
+                {categories?.fetching ? (
+                  <button
+                    className="button"
+                    style={{
+                      width: '100%',
+                      border: 'none',
+                      borderRadius: 0,
+                      cursor: 'pointer',
+                    }}
+                  >
+                    <Puff
+                      height="20"
+                      width="20"
+                      radius={1}
+                      color="#0f3058"
+                      ariaLabel="puff-loading"
+                      wrapperStyle={{ display: 'flex', justifyContent: 'center' }}
+                      wrapperClass=""
+                      visible={true}
+                    />
+                  </button>
+                ) : (
+                  <Link to={'/'}>
+                    <button
+                      className="button"
+                      onClick={() => fetchMeals(cat)}
+                      style={{
+                        width: '100%',
+                        border: 'none',
+                        borderRadius: 0,
+                        cursor: 'pointer',
+                      }}
+                    >
+                      {cat}
+                    </button>
+                  </Link>
+                )}
+              </li>
+            );
+          })}
           <li>
             <a href="/">Vegan</a>
           </li>

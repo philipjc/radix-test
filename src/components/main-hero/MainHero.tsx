3f50dcda71faf32432b82ae1ace7847401d2f680
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-11-05 17:41:54 +0000

    Using actual API data for meal list.

--- src/components/main-hero/MainHero.tsx
+++ src/components/main-hero/MainHero.tsx
@@ -23,8 +23,8 @@ export function MainHero(): ReactElement {
         <DarkerDMSectionStyled className="hero is-large main-hero column is-12 m-auto">
           <div className="hero-body is-flex is-justify-content-space-between p-0 pl-6 pr-6 pt-6 pb-6">
             <div className="container is-flex is-flex-direction-column is-align-items-flex-start">
-              <p className="title">Foode..</p>
-              <p className="subtitle">Food for all the foode's</p>
+              <h2 className="title is-2">Foode..</h2>
+              <h5 className="subtitle is-5">Food for all the foode's</h5>
             </div>
             <figure className="image column p-0">
               <LazyImage />
@@ -34,21 +34,43 @@ export function MainHero(): ReactElement {
           <div className="hero-foot">
             <nav className="tabs is-boxed is-fullwidth">
               <div className="container">
-                <ul>
-                  <li className="is-active">
+                <ul className="is-flex is-justify-content-space-evenly">
+                  <li className="is-active is-flex-grow-1">
                     {categories?.fetching ? (
-                      <Puff
-                        height="20"
-                        width="20"
-                        radius={1}
-                        color="#0f3058"
-                        ariaLabel="puff-loading"
-                        wrapperStyle={{ display: 'flex', justifyContent: 'center' }}
-                        wrapperClass=""
-                        visible={true}
-                      />
+                      <button
+                        className="button"
+                        onClick={() => fetchMeals()}
+                        style={{
+                          width: '100%',
+                          border: 'none',
+                          borderRadius: 0,
+                          cursor: 'pointer',
+                        }}
+                      >
+                        <Puff
+                          height="20"
+                          width="20"
+                          radius={1}
+                          color="#0f3058"
+                          ariaLabel="puff-loading"
+                          wrapperStyle={{ display: 'flex', justifyContent: 'center' }}
+                          wrapperClass=""
+                          visible={true}
+                        />
+                      </button>
                     ) : (
-                      <button onClick={() => fetchMeals()}>{veg?.strCategory}</button>
+                      <button
+                        className="button"
+                        onClick={() => fetchMeals()}
+                        style={{
+                          width: '100%',
+                          border: 'none',
+                          borderRadius: 0,
+                          cursor: 'pointer',
+                        }}
+                      >
+                        {veg?.strCategory}
+                      </button>
                     )}
                   </li>
                   <li>
--- src/components/meals-list/MealsList.tsx
+++ src/components/meals-list/MealsList.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import { useMeals } from '../../features/general/hooks/useMeals';
 import { iUseMeals } from '../../features/general/hooks/useMeals';
 import sharedStyled from '../../shared-styled/SharedStyled';
+import { iMeal } from '../../features/general/api/mealsByCategory';
 
 const { DarkerDMSectionStyled } = sharedStyled;
 
@@ -19,40 +20,25 @@ export function MealsList(): ReactElement {
     <DarkerDMSectionStyled className="section">
       <div className="column is-10 ml-auto mr-auto">
         <div className="is-flex is-justify-content-space-between is-flex-wrap-wrap">
-          {[1, 2, 3, 4, 5, 6].map(num => (
+          {meals.map((meal: iMeal) => (
             <div
               className="card mr-2 ml-2 is-flex-grow-1"
               style={{ maxWidth: 250, marginBottom: 80 }}
             >
               <div className="card-image">
                 <figure className="image is-4by3">
-                  <img
-                    src="https://bulma.io/images/placeholders/1280x960.png"
-                    alt="Placeholder image"
-                  />
+                  <img src={meal.strMealThumb} alt="Placeholder image" />
                 </figure>
               </div>
               <div className="card-content">
                 <div className="media">
                   <div className="media-left">
-                    <figure className="image is-48x48">
-                      <img
-                        src="https://bulma.io/images/placeholders/96x96.png"
-                        alt="Placeholder image"
-                      />
-                    </figure>
-                  </div>
-                  <div className="media-content">
-                    <p className="title is-4">John Smith</p>
-                    <p className="subtitle is-6">@johnsmith</p>
+                    <p className="title is-4 is-flex is-align-content-start">{meal.strMeal}</p>
                   </div>
                 </div>
 
                 <div className="content">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis
-                  mauris. <a>@bulmaio</a>.<a href="#">#css</a> <a href="#">#responsive</a>
-                  <br />
-                  <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                  <p>Click the card to checkout this lovely meal</p>
                 </div>
               </div>
             </div>

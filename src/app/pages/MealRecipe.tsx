6a10c53d1dc8a068a598fe78632fdbb540a774e2
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-11-05 20:35:25 +0000

    Aligning image and ingredients for recipe page card

--- src/app/pages/MealRecipe.tsx
+++ src/app/pages/MealRecipe.tsx
@@ -22,40 +22,36 @@ export function MealRecipe(): ReactElement {
     <DarkerDMSectionStyled className="section">
       <div className="column is-10 m-auto has-text-left">
         <div className="card">
-          <div className="card-content">
-            <div className="media is-flex is-flex-direction-column is-align-items-center p-6">
-              <div className="media-content has-text-centered mb-5">
-                <p className="title is-4">{recipe['strMeal']}</p>
-              </div>
-              <figure className="image is-128x128 mb-5">
-                <img
-                  className="is-rounded is-responsive"
-                  src={recipe['strMealThumb']}
-                  alt="Placeholder image"
-                />
+          <div className="column is-flex">
+            <div className="card-image column">
+              <figure className="is-4by3">
+                <img src={recipe['strMealThumb']} alt="Placeholder image" />
               </figure>
             </div>
+            <div className="column has-text-centered">
+              <p className="title is-4">{recipe['strMeal']}</p>
+              <h3>Ingredients</h3>
+              <ul className="ml-0 pl-4">
+                {ingredientsKeysArray.map((ingredient: string, idx: number) => {
+                  return (
+                    ingredient && (
+                      <li className="ml-0 pl-0" key={`ingredient-${idx}`}>
+                        {ingredient}
+                      </li>
+                    )
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
 
-            <div className="content is-flex p-2">
+          <div className="card-content">
+            <div className="content is-flex p-6">
               <div className="column">
                 <h3>Instructions</h3>
                 {recipe['strInstructions'].split('.').map((line: string, idx: number) => (
                   <p key={`Instruction-line-${idx}`}>{line}</p>
                 ))}
-              </div>
-              <div className="column is-3 ml-6">
-                <h3>Ingredients</h3>
-                <ul className="ml-0 pl-4">
-                  {ingredientsKeysArray.map((ingredient: string, idx: number) => {
-                    return (
-                      ingredient && (
-                        <li className="ml-0 pl-0" key={`ingredient-${idx}`}>
-                          {ingredient}
-                        </li>
-                      )
-                    );
-                  })}
-                </ul>
               </div>
             </div>
           </div>

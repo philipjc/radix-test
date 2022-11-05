b29f63deeedec311c13561e77a2a1203c84539a6
Philip Cox <80514248+philc-homehub@users.noreply.github.com>
2022-11-05 22:39:30 +0000

    using random web image until assets on cloud.

--- src/components/lazy-image/LazyImage.tsx
+++ src/components/lazy-image/LazyImage.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import MainHeroImage from '../../assets/images/hero-food-chop.jpg';
+// import MainHeroImage from '../../assets/images/hero-food-chop.jpg';
 
 export function LazyImage() {
   return (
-    <img loading="lazy" src={MainHeroImage} alt="A table with plates and bowls of food and herbs" />
+    <img
+      loading="lazy"
+      src="https://picsum.photos/200/?grayscale/?blur"
+      alt="A table with plates and bowls of food and herbs"
+    />
   );
 }

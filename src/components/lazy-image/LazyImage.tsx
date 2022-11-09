c308dbb324b0a411ad604fa90b0808d67cad2836
Philip Cox <philipcox83@gmail.com>
2022-11-09 20:29:28 +0000

    changed image ipsum for bacon!

--- src/components/lazy-image/LazyImage.tsx
+++ src/components/lazy-image/LazyImage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 // import MainHeroImage from '../../assets/images/hero-food-chop.jpg';
 
+// const billMurrayPic = 'https://www.fillmurray.com/640/360';
+const baconPic = 'https://baconmockup.com/640/360';
+// const randomPic = 'https://loremflickr.com/640/360';
+
 export function LazyImage() {
-  return (
-    <img
-      loading="lazy"
-      src="https://picsum.photos/200/?grayscale/?blur"
-      alt="A table with plates and bowls of food and herbs"
-    />
-  );
+  return <img loading="lazy" src={baconPic} alt="A place holder pic" />;
 }
+
+// 80514248+philc-homehub@users.noreply.github.com

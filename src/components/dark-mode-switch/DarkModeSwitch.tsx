7c3cd43f5db15678ccbd0d42e0226e9a6de2437f
Philip Cox <philipcox83@gmail.com>
2022-11-22 11:58:40 +0000

    Darm mode icons added.

--- src/components/dark-mode-switch/DarkModeSwitch.tsx
+++ src/components/dark-mode-switch/DarkModeSwitch.tsx
@@ -1,23 +1,28 @@
 import { ReactElement } from 'react';
-import { useAppDispatch } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { darkMode } from '../../features/general/state/actions';
+import { selectDarkMode } from '../../features/general/state/accessors';
 import darkModeSwitchStyles from './dark-mode-switch.styled';
 import SharedStyles from '../../shared-styled/SharedStyled';
 
 const { DMSectionStyled } = SharedStyles;
-const { Flex, Label, StyledSwitch, StyledThumb } = darkModeSwitchStyles;
+const { Flex, StyledSwitch, StyledThumb } = darkModeSwitchStyles;
 
 export function DarkModeSwitch(): ReactElement {
   const dispatch = useAppDispatch();
+  const isDarkMode = useAppSelector(selectDarkMode);
 
   return (
     <DMSectionStyled className="section pt-2 pb-2">
       <div className="column is-10 ml-auto mr-auto">
         <form>
           <Flex>
-            <Label htmlFor="s1">Dark mode</Label>
             <StyledSwitch id="s1" onCheckedChange={() => dispatch(darkMode())}>
-              <StyledThumb />
+              <StyledThumb>
+                {isDarkMode ? <FontAwesomeIcon icon={faSun} /> : <FontAwesomeIcon icon={faMoon} />}
+              </StyledThumb>
             </StyledSwitch>
           </Flex>
         </form>

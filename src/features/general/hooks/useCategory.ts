7d2fec4e188af350609062fbd0713bfddcbdb968
Philip Cox <philipcox83@gmail.com>
2022-11-24 11:41:17 +0000

    types useCategory

--- src/features/general/hooks/useCategory.ts
+++ src/features/general/hooks/useCategory.ts
@@ -1,8 +1,19 @@
 import { selectUserCategoriesState } from '../state/accessors';
 import { useAppSelector } from '../../../app/hooks';
 import { changeCurrentCategory, removeCategory } from '../state/actions';
+import { iCategoryModel } from '../state/generalStateModel';
+import { iCurrentCategory } from '../state/interfaces/index.js';
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
 
-export function useCategory() {
+interface iUseCategory {
+  // length: () => number;
+  categories: {}[];
+  current: iCurrentCategory | null;
+  AChangeCategory: ActionCreatorWithPayload<iCurrentCategory>;
+  ARemoveCategory: ActionCreatorWithPayload<number>;
+}
+
+export function useCategory(): iUseCategory {
   const { categories, current } = useAppSelector(selectUserCategoriesState);
 
   return {
